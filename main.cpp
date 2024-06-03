20ec90ea6df8b718050746ce8a754ddf50cc4ba8
junios <junios@naver.com>
2024-06-03 11:36:10 +0900

    vector로 자료 구조 바꿈

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -17,24 +18,39 @@ int main()
 	int SlimeCount = rand() % 3 + 1;
 	int BoarCount = rand() % 3 + 1;
 
-	FGoblin* Goblins = new FGoblin[GoblinCount];
-	FSlime* Slimes = new FSlime[SlimeCount];
-	FBoar* Boars = new FBoar[BoarCount];
+	vector<FGoblin*> Goblins;
+	vector<FSlime*> Slimes;
+	vector<FBoar*> Boars;
+
+	for (int i = 0; i < GoblinCount; ++i)
+	{
+		Goblins.push_back(new FGoblin());
+	}
+
+	for (int i = 0; i < SlimeCount; ++i)
+	{
+		Slimes.push_back(new FSlime());
+	}
+
+	for (int i = 0; i < BoarCount; ++i)
+	{
+		Boars.push_back(new FBoar());
+	}
 
 	while (true)
 	{
 		Player->Move();
-		for (int i = 0; i < GoblinCount; ++i)
+		for (int i = 0; i < Goblins.size(); ++i)
 		{
-			(rand() % 2 == 0) ? Goblins[i].Move() : Goblins[i].Attack();
+			(rand() % 2 == 0) ? Goblins[i]->Move() : Goblins[i]->Attack();
 		}
-		for (int i = 0; i < SlimeCount; ++i)
+		for (int i = 0; i < Slimes.size(); ++i)
 		{
-			(rand() % 2 == 0) ? Slimes[i].Move() : Slimes[i].Attack();
+			(rand() % 2 == 0) ? Slimes[i]->Move() : Slimes[i]->Attack();
 		}
-		for (int i = 0; i < BoarCount; ++i)
+		for (int i = 0; i < Boars.size(); ++i)
 		{
-			(rand() % 2 == 0) ? Boars[i].Move() : Boars[i].Attack();
+			(rand() % 2 == 0) ? Boars[i]->Move() : Boars[i]->Attack();
 		}
 	}
 
@@ -42,9 +58,23 @@ int main()
 
 
 	delete Player;
-	delete[] Goblins;
-	delete[] Slimes;
-	delete[] Boars;
+	for (FGoblin* Goblin : Goblins)
+	{
+		delete Goblin;
+	}
+	Goblins.clear();
+
+	for (auto Slime : Slimes)
+	{
+		delete Slime;
+	}
+	Slimes.clear();
+
+	for (int i = 0; i < Boars.size(); ++i)
+	{
+		delete Boars[i];
+	}
+	Boars.clear();
 
 	return 0;
 }
